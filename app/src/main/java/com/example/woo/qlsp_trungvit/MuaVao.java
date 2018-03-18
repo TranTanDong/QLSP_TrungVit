15733c866c9fb26a826f620a59cb51e59fa3d888
TranTanDong <hanraewoo1001@gmail.com>
2018-03-18 20:07:04 +0700

    OptionMenu

--- app/src/main/java/com/example/woo/qlsp_trungvit/MuaVao.java
+++ app/src/main/java/com/example/woo/qlsp_trungvit/MuaVao.java
@@ -1,7 +1,11 @@
 package com.example.woo.qlsp_trungvit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MuaVao extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MuaVao extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mua_vao);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+
+    }
+
+    private void addControls() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_add, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add){
+            Intent intent = new Intent(MuaVao.this, ThongKe.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
